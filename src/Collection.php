b2ab2a2e5f53f6b249172713b0ceb354d1dfbe24
lackoxygen <jingze666@gmail.com>
2023-11-08 10:14:20 +0800

    Remove redundant methods

--- src/Collection.php
+++ src/Collection.php
@@ -1544,29 +1544,4 @@ class Collection implements ArrayAccess, Countable
     {
         unset($this->items[$key]);
     }
-
-    public function current()
-    {
-        // TODO: Implement current() method.
-    }
-
-    public function next()
-    {
-        // TODO: Implement next() method.
-    }
-
-    public function key()
-    {
-        // TODO: Implement key() method.
-    }
-
-    public function valid()
-    {
-        // TODO: Implement valid() method.
-    }
-
-    public function rewind()
-    {
-        // TODO: Implement rewind() method.
-    }
 }
